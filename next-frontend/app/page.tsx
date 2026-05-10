b5ea865fa7957f797a2112a38540a6694b264a3a
Luiz Carlos <argentinaluiz@gmail.com>
2026-05-10 11:13:30 -0300

    instalação e configuração do shadcn

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-//tailwindcss - utility first -> problemas cascatas
+import { Button } from "../components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button size="lg">
+        Hello World
+      </Button>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
